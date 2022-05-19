3b28579de0a33f511aa50eb09b032ee8fdb19a26
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-05-19 13:14:21 -0600

    An element from my first card component is rendering to the DOM, integrated a config.js to hold tokens

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-// import { hot } from 'react-hot-loader/root';
+import Card from "./Related/Card.jsx"
 
 class App extends React.Component {
   render() {
-   // const { name } = 'Robert';
     return (
       <>
         <h1>
           Hello Robert
         </h1>
+        <Card />
       </>
     );
   }
 }
 
 export default App = App;
-// export default hot(App);
---
+++ client/src/components/Related/Card.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+var Card = (props) => {
+
+
+  return (
+    <div id="card">
+      Hello Card!
+    </div>
+  )
+}
+
+export default Card = Card;
