e8cb8a4c1f08696887a70f157a4c3cc247912142
Michael Lin <michaelin.96@gmail.com>
2022-05-19 11:15:46 -0700

    edit RnR Widget name

---
+++ client/src/components/R&R/ReviewWidget.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useContext, createContext, useEffect} from 'react';
+import axios from 'axios';
+
+
+export default function ReviewWidget () {
+
+
+
+  return (
+    <div className="review-widget">
+
+    </div>
+  );
+};
