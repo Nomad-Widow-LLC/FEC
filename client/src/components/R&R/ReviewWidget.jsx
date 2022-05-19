94fcaf3a169164613c960f0d8d23396dfc18bc02
Michael Lin <michaelin.96@gmail.com>
2022-05-19 14:37:09 -0700

    Able to Access API

--- client/src/components/R&R/ReviewWidget.jsx
+++ client/src/components/R&R/ReviewWidget.jsx
@@ -1,19 +1,24 @@
 import React, {useState, useContext, createContext, useEffect} from 'react';
 import axios from 'axios';
-
+import config from '../../../dist/config.js';
 
 
 export default function ReviewWidget () {
 
-  useEffect(() => {
-   axios.get('/reviews')
-    .then((data) => {
-      return data;
-    })
-    .catch((err) => {
-      console.log('Could Not Access Atelier API');
-    });
-  }, [])
+
+
+
+
+  // useEffect(() => {
+  //   axios.get(`${config.API_PATHNAME}reviews/?product_id=2`, {headers: {Authorization: `${config.GITHUB_API}` }})
+  //     .then((data) => {
+  //       console.log(data);
+  //     })
+  //     .catch((err) => {
+  //       console.log('Could Not Access Atelier API');
+  //     });
+
+  // }, [])
 
   return (
     <div className="review-widget">
