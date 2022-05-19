e416fbeab79df7b09c57cc65656365b494ca2300
Donna Szeto <szetodonna2@gmail.com>
2022-05-19 15:13:28 -0700

    added two new components: star and star rating to create star rating in product info

---
+++ client/src/components/Overview/ProductInformation/Star.jsx
@@ -0,0 +1,20 @@
+import React, {useState, useMemo} from 'react'
+import { FaStar } from 'react-icons/fa';
+
+const Star = ({index, rating}) => {
+
+  const fill = useMemo(() => {
+    if (rating >= index) {
+      return 'yellow';
+    } else {
+      return 'black';
+    }
+  }, [index, rating]);
+
+  return (
+    <FaStar fill={fill}/>
+  )
+
+}
+
+export default Star;
--- client/src/components/Overview/ProductInformation/StarRating.jsx
+++ client/src/components/Overview/ProductInformation/StarRating.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import { FaStar } from 'react-icons/fa'
+import React, {useState, useEffect} from 'react';
+import Star from './Star.jsx';
 
 const StarRating = () => {
+  // states:
+  // Rating, setRating - this is determined from the data, this will determine how many stars are colored
+  const [rating, setRating] = useState(0);
+
+  // get request to get rating of product and set state of rating
+  useEffect(() => {
+    setRating(4);
+  })
+
   return (
     <div className='StarRating'>
-      <FaStar />
+      {[1, 2, 3, 4, 5].map((index) => {
+        return (
+          <Star index={index} rating={rating} key={index}/>
+        )
+      })}
     </div>
   )
 }
