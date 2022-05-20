68d29e5d7e125b172fdd5b0b21d717daf7e91498
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-05-20 16:55:49 -0600

    Commiting more changes so I can switch to my branch

---
+++ client/src/components/Related/Card.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import  useToGetProducts from './lib/hooks.js';
+
+var Card = (props) => {
+
+
+  return (
+    <div id="card">
+      Hello Card!
+    </div>
+  )
+}
+
+export default Card = Card;
