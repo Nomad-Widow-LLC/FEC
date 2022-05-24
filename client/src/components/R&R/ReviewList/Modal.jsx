7d115deddd1a09c19e89aa93e4d6c3c6d81252ac
Michael Lin <michaelin.96@gmail.com>
2022-05-23 20:00:05 -0700

    Added Portal Functionally and Styles

---
+++ client/src/components/R&R/ReviewList/Modal.jsx
@@ -0,0 +1,59 @@
+import React, {useState, useContext, createContext, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import {FaTimes} from 'react-icons/fa';
+
+
+const ModalStyles = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #FFF;
+  padding: 25px;
+  zIndex: 1000;
+`
+const OverlayStyles = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  zIndex: 1000;
+`
+const ExpandedImg = styled.img`
+  width: 400px;
+  height: auto;
+`
+const Icon = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  opacity: 0.7;
+  cursor: pointer;
+  &:hover {
+    opacity: 1.0
+  }
+`
+
+export default function Modal({open, photo, onClose}) {
+
+  if (!open) return null
+
+  return ReactDOM.createPortal(
+    <>
+      <OverlayStyles>
+        <ModalStyles>
+          <div>
+            <ExpandedImg src={photo.url} alt={photo.id}></ExpandedImg>
+            <Icon>
+              <FaTimes onClick={onClose}/>
+            </Icon>
+          </div>
+        </ModalStyles>
+      </OverlayStyles>
+    </>,
+    document. getElementById('portal')
+  );
+}
--- client/src/components/R&R/ReviewList/ReviewPhotos.jsx
+++ client/src/components/R&R/ReviewList/ReviewPhotos.jsx
@@ -1,15 +1,26 @@
 import React, {useState, useContext, createContext, useEffect} from 'react';
 import styled from 'styled-components';
+import Modal from './Modal.jsx';
 
 const PhotoFlex = styled.div`
   margin: 2px;
 `
 
+const Thumbnail = styled.img`
+  width: 75px;
+  height: auto;
+`
+
 export default function ReviewPhotos ({photo}) {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+
   return(
     <PhotoFlex>
       <div className="photo-entry">
-        <img src={photo.url} alt={photo.id} width="75" height="auto" ></img>
+        <Thumbnail src={photo.url} alt={photo.id} onClick={() => setIsOpen(true)}></Thumbnail>
+        <Modal open={isOpen} photo={photo} onClose={() => setIsOpen(false)} />
       </div>
     </PhotoFlex>
   );
