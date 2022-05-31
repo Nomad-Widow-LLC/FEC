ac6c60b9b70eb12dc3530d8cf6fad00acaccbe67
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-05-31 13:38:47 -0600

    Commit before updating

--- client/src/components/Related/Card.jsx
+++ client/src/components/Related/Card.jsx
@@ -6,43 +6,38 @@ import StarRating from "../Overview/ProductInformation/StarRating.jsx";
 
 var Card = ({pic, item, salePrice, stars}) => {
   let starBar;
+  let sale;
 
   if (stars) {
     starBar = <StarRating className="stars price" rating={stars.avg} />;
   }
 
-  if (salePrice === null) {
-    return (
-      <div className="card-container">
-      <div className="card">
-        <img className="img" src={pic} />
-        <div className="info">
-          <div className="name">{item.name}</div>
-          <div className="category">{item.category}</div>
-          <span className="price">${item.default_price}</span>
-          {starBar}
-        </div>
-      </div>
-    </div>
-    )
-  } else if (salePrice !== null) {
-    return (
-      <div className="card-container">
-      <div className="card">
-        <img className="img" src={pic} />
-        <div className="info">
-          <div className="name">{item.name}</div>
-          <div className="category">{item.category}</div>
-          <span className="priceContainer">
+  if (salePrice) {
+    sale = <span className="priceContainer">
             <span className="price redSale">${salePrice}</span>
             <span className="price lineThrough">${item.default_price}</span>
             {starBar}
-        </span>
+           </span>
+  } else {
+    sale = <span className="priceContainer">
+            <span className="price">${item.default_price}</span>
+            {starBar}
+           </span>
+  }
+
+  return (
+    <div className="card-container">
+      <div className="card">
+        <img className="img" src={pic} />
+        <div className="info">
+          <div className="name">{item.name}</div>
+          <div className="category">{item.category}</div>
+          {sale}
         </div>
       </div>
     </div>
-    )
-  }
+  )
+
 }
 
 export default Card = Card;
