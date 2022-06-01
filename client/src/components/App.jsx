208d33b5dbbaf8b846c800b2135e90145762839a
Donna Szeto <szetodonna2@gmail.com>
2022-05-31 17:32:55 -0700

    delete commit  Please enter the commit message for your changes. Lines starting

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -24,6 +24,7 @@ export default function App () {
         <div className="componentGap" />
         <Carousel />
         <div className="componentGap" />
+        <a id="review-widget"></a>
         <ReviewWidget />
       </AllProductInfo.Provider>
     </>
