c5661819f3bea38293f382c8013b17c83a9b0e3b
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-06-03 14:49:59 -0600

    Saving before merging main

--- client/src/components/Related/Outfit.jsx
+++ client/src/components/Related/Outfit.jsx
@@ -9,6 +9,38 @@ let Outfit = () => {
 
   let {overviewProduct, overviewStyle, outfitCarousel, setOutfitCarousel} = useContext(CarouselStates);
 
+  const clicker = (mode) => {
+    if (mode === 'prev') {
+
+      // This makes sure not to scroll past the first item
+      if (sectionIndex !== -1) {
+        setSectionIndex(sectionIndex -= 1)
+      }
+      setJump(jump = sectionIndex * cardWidth);
+
+      //logAll([`Carousel Width: ${carouselWidth}`, `Number Of Cards Displayed: ${nCardsDisplayed}`, `Idex Offset: ${indexOffset}`, `Section Index: ${sectionIndex}`, `Jump: ${jump}`, `===============`]);
+
+      // Finding the track on the DOM
+      let track = document.querySelector(`.track`);
+      // Transforming the carousel according to how far it needs to jump
+      track.style.transform = `translateX(-${jump}px)`;
+
+    } else if (mode === 'next') {
+      if (nCardsDisplayed + sectionIndex !== productList.length) {
+        setSectionIndex(sectionIndex += 1);
+      }
+
+      setJump(jump = sectionIndex * cardWidth);
+
+      //logAll([`Carousel Width: ${carouselWidth}`, `Number Of Cards Displayed: ${nCardsDisplayed}`, `Idex Offset: ${indexOffset}`, `Section Index: ${sectionIndex}`, `Jump: ${jump}`, `# of Products: ${productList.length}`, `===============`]);
+
+      let track = document.querySelector(`.track`);
+      track.style.transform = `translateX(-${jump}px)`;
+    } else {
+      console.log(`Danger in clicker Will Robinson!`)
+    }
+  }
+
   useEffect(() => {
 
   }, [outfitCarousel])
@@ -20,8 +52,8 @@ let Outfit = () => {
         <div className="title">Your Outfit</div>
         <div className="spacer" />
         <div className="nav" key="outfitnav">
-          <FaArrowLeft className="prev button" onClick={() => {console.log(overviewProduct);}} />
-          <FaArrowRight className="next button" onClick={() => {console.log(outfitCarousel); console.log(overviewStyle)}} />
+          <FaArrowLeft className="prev button" onClick={() => {clicker('prev')}} />
+          <FaArrowRight className="next button" onClick={() => {clicker('next')}} />
         </div>
         <div className="inner-carousel" key="inner">
           <div className="track" key="outfittrack">
